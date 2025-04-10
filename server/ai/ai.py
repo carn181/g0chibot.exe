dd7d2119898b206e58b230a3a84e5515cce9f598
carn181 <carn181@proton.me>
2025-04-10 12:00:22 -0400

    Add Chatbot Class

--- server/ai/ai.py
+++ server/ai/ai.py
@@ -3,16 +3,30 @@ from google.genai import types
 import os
 
 key=os.environ.get('GEMINI_API_KEY')
-print(key)
+print("Your API_Key is: "+key)
 client = genai.Client(api_key=key)
 
-chat = client.chats.create(model="gemini-2.0-flash",
-    config=types.GenerateContentConfig(
-        system_instruction="For any of the following messages, speak like a 18 year old l33t hacker boy on the 2000's internet. I will communicate with you like I'm on MySpace and you should respond like then. You should give me a fun fact of the 2000's internet as well when I ask. Never give huge paragraphs, and give new sentences on new lines.")
-    )
+
+class Chatbot:
+    def __init__(self, name, instr):
+        self.name=name
+        self.instr=instr
+        self.chat=None
+        
+    def init(self):
+        self.chat= client.chats.create(model="gemini-2.0-flash",
+                                       config=types.GenerateContentConfig(
+                                           system_instruction=self.instr))
+    def send_msg(self,msg):
+        response=self.chat.send_message(msg)
+        return response
+
+
+chat=Chatbot("2000's guy", "For any of the following messages, speak like a 18 year old l33t hacker boy on the 2000's internet. I will communicate with you like I'm on MySpace and you should respond like then. You should give me a fun fact of the 2000's internet as well when I ask. Never give huge paragraphs, and give new sentences on new lines.")
+chat.init()
 
 for i in range(10):
     message=input("Input: ")
-    response = chat.send_message(message)
+    response = chat.send_msg(message)
     print(response.text)
 
