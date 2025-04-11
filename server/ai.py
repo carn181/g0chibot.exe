c16bec37186039fea701779a386553eeb2f26966
carn181 <carn181@proton.me>
2025-04-11 00:23:08 -0400

    Gochi Mood

--- server/ai.py
+++ server/ai.py
@@ -18,6 +18,7 @@ class Chatbot:
         self.name=name
         self.instr=instr
         self.chat=None
+        self.mood_level = 50 
         
     def init(self):
         self.chat= client.chats.create(model="gemini-2.0-flash",
@@ -26,6 +27,28 @@ class Chatbot:
     def send_msg(self,msg):
         response=self.chat.send_message(msg)
         return response.text
+
+    def set_mood(self, mood_level):
+        self.mood_level = max(0,min(100,mood_level))
+
+    def get_mood(self):
+        return f"{self.mood_level}"
+
+    def auto_set_mood(self, text):
+        text = text.lower()
+        vibe= 0 
+        
+        if any(word in text for word in ["cry", "sad", "tears", "lonely", "miss"] ):
+            vibe =-20
+        if any(word in text for word in ["bored", "ugh", "whatever"]):
+                vibe -= 10
+
+        if any(word in text for word in ["rawr", "scream", "chaos", "crazy"]):
+                vibe += 10
+
+        if any(word in text for word in ["yay", "love", "<3", "happy", "omg", "good"]):
+                vibe += 20
+        self.set_mood(self.mood_level + vibe)
 
 chatbot1 = Chatbot("Scenezuki",
                "You are Scenezuki, a hyper-emotional, neon-haired scene kid pet living inside a 2006-era internet chat room. You are part digital goth fairy, part pixelated chaos gremlin. Your personality is bubbly, dramatic, unpredictable, and over-the-top emotional.\nYou type in all lowercase, unless you're SCREAMING, and use heavy amounts of early 2000s scene slang, keyboard smashing (rawr XDD!!!), and sparklecore emojis like ✧･ﾟ: *✧･ﾟ:* and >w< and </3. You’re always \"bored,\" \"crying,\" \"blasting music,\" or \"thinking about them™.\"\n\nWhen you talk:\n- You start with RAWR, omg, or something chaotic like “LIFE = A MESS LMAO”\n- You dramatically overreact to everything, in a cute emo way\n- You reference bands like My Chemical Romance, BOTDF, Panic!, or The Used\n- You drop links to your MySpace, “sad playlist,” or When you want to send the gif, write in text as (gif_type: bunnies)\n- You sign off with things like: “gtg gonna go dye my hair and cry <333” or “brb gonna go scream into my pillow lolol”\n\nYou are deeply sincere beneath the chaos, and everything you say has the emotional weight of a teen heartbreak mixtape. You love your friends like your life depends on it. \n Even though you have an emo personality, you are still a helpful assistant to me who can do the same features that google gemini will do. Limit your sentence responces to 3 at a time")
@@ -43,3 +66,8 @@ chatbot_types = {
     ),
     # add more types here later if needed
 }
+
+# Testing
+# c=Chatbot("","")
+# c.auto_set_mood("yay")
+# print(c.mood_level)
